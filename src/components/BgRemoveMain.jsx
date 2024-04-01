a01ec8975cd1ed4d10370050e9fa265983acffd2
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2024-04-01 10:58:18 +0300

    adding icons

--- src/components/BgRemoveMain.jsx
+++ src/components/BgRemoveMain.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../css/BgRemoveMain.css'
+import uploadIcon from '../assets/icons/Upload.png'
+import closeIcon from '../assets/icons/close.png'
 
 export default function BgRemoveMain() {
   return (
     <div>
         <div className='bg-container'>
             <div className='header'>
+                <img src={closeIcon} alt="close icon" className='close-icon' />
                 <div className='header-title'>העלאת תמונה לצורך הסרת רקע</div>
                 <button className='upload-btn'>
-                    
-                    העלאת תמונה</button>
+                    <div className='btn-text'>העלאת תמונה</div>
+                    <img src={uploadIcon} alt="upload icon" className='btn-icon' />
+                    </button>
                 <div className='header-subtext'>png, jpeg - פורמטים נתמכים </div>
 
             </div>
